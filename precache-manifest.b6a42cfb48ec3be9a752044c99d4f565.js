00d9986c8de718f1926d4720de83d36c64253b79
djrhernandez <djrhernandez@yahoo.com>
2019-01-17 18:37:09 -0800

    Updates

---
+++ precache-manifest.b6a42cfb48ec3be9a752044c99d4f565.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "8f1c46abd9df938ca325",
+    "url": "/portfolio/static/js/main.8f1c46ab.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/portfolio/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "8f1c46abd9df938ca325",
+    "url": "/portfolio/static/css/main.13d4ee8c.chunk.css"
+  },
+  {
+    "revision": "f007938d3f94f2c54cb84d377376b3b7",
+    "url": "/portfolio/index.html"
+  }
+];
