a30c738ebc3f7ac5a76c6042c5dbd087b0fa0365
djrhernandez <djrhernandez@yahoo.com>
2019-01-21 17:21:23 -0800

    Updating index with scripts, stylesheet links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/header/header';
+import About from './components/about/about';
+import Resume from './components/resume/resume';
+import Projects from './components/projects/projects';
+import Contact from './components/contact/contact';
+import Footer from './components/footer/footer';
 
 class App extends Component {
+	constructor() {
+		super();
+
+		var today = new Date();
+		var date = `${today.getMonth()+1}/${today.getDate()}/${today.getFullYear()}}`;
+
+		this.state = {
+			date: date
+		}
+	}
+
     render() {
         return (
             <div className="App">
-                <header className="App-header">
+				<Header>
+				<header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
                     <p> Edit <code>src/App.js</code> and save to reload. </p>
                     <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
@@ -18,3 +34,14 @@ class App extends Component {
     }
 }
 export default App;
+/*
+<div className="App">
+	<Header>
+	<About>
+	<Resume>
+	<Projects>
+	<Contact>
+	<Footer>
+</div>
+
+*/
