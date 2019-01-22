5462fe8117501736fad952f35f2529c09f279a11
djrhernandez <djrhernandez@yahoo.com>
2019-01-21 17:24:36 -0800

    Updating app.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,6 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-				<Header>
 				<header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
                     <p> Edit <code>src/App.js</code> and save to reload. </p>
