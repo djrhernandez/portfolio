a6d9f1bc303997a8e6f124b33f9c3859fb6e27cc
djrhernandez <djrhernandez@yahoo.com>
2019-02-15 09:09:01 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.bcc3285ab341f53d89b2ca8413ef5f43.js"
+  "/portfolio/precache-manifest.4732ff56162335f9917e4f6aea40f9e9.js"
 );
 
 workbox.clientsClaim();
